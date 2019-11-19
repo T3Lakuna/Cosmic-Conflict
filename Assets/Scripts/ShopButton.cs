3b9a4ee34128faead01439bdcd0a90bfb5d4a02c
CheesiePants <fernandowesome@gmail.com>
2019-11-19 14:29:34 -0600

    no message

---
+++ Assets/Scripts/ShopButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopButton : MonoBehaviour
+{
+    public Image shop;
+    void Start()
+    {
+        shop.gameObject.SetActive(false);
+    }
+
+    public void shopPress()
+    {
+        shop.gameObject.SetActive(true);
+    }
+
+    public void exitButton()
+    {
+        shop.gameObject.SetActive(false);
+    }
+}
