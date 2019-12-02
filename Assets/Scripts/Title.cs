5256a844a524f09904bf6f90852b61cef6cf9d5f
BuildTools <unconfigured@null.spigotmc.org>
2019-12-01 23:31:13 -0600

    UI elements for Title screen and collection

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Title : MonoBehaviour
+{
+    public GameObject titleScreen;
+    public GameObject heroScreen;
+    void Start()
+    {
+        titleScreen.gameObject.SetActive(true);
+        heroScreen.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void toHeroScreen()
+    {
+        titleScreen.gameObject.SetActive(false);
+        heroScreen.gameObject.SetActive(true);
+    }
+
+    public void toTitleScreen()
+    {
+        titleScreen.gameObject.SetActive(true);
+        heroScreen.gameObject.SetActive(false);
+    }
+}
