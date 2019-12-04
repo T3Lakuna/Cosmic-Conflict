d0b93a75df1c5a026422674ca51aa7e722d6837b
CheesiePants <fernandowesome@gmail.com>
2019-12-04 14:27:12 -0600

    Poopy

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Title : MonoBehaviour {
+	public GameObject titleScreen;
+	public GameObject heroScreen;
+	public GameObject optionScreen;
+	public GameObject theHero1;
+
+	private void Start() {
+		this.titleScreen.gameObject.SetActive(true);
+		this.heroScreen.gameObject.SetActive(false);
+	}
+
+	public void ToHeroScreen() {
+		this.titleScreen.gameObject.SetActive(false);
+		this.heroScreen.gameObject.SetActive(true);
+	}
+
+	public void ToTitleScreen() {
+		this.titleScreen.gameObject.SetActive(true);
+		this.heroScreen.gameObject.SetActive(false);
+	}
+
+	public void ToOptions() {
+		this.titleScreen.gameObject.SetActive(false);
+		this.optionScreen.gameObject.SetActive(true);
+	}
+
+	public void BackToHero() {
+		this.heroScreen.gameObject.SetActive(true);
+		this.theHero1.gameObject.SetActive(false);
+	}
+
+	public void ToHero1() {
+		this.heroScreen.gameObject.SetActive(false);
+		this.theHero1.gameObject.SetActive(true);
+	}
+}
