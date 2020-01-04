9b3db7fe008a6901a7f57f54ec70eba5563fc5a1
Travis Martin <tjmartin2003@gmail.com>
2020-01-04 02:07:17 -0800

    Add [nexus plural].

---
+++ Assets/Resources/Structures/Nexus/Nexus.cs
@@ -0,0 +1,9 @@
+﻿public class Nexus : Structure {
+	public Team inspectorTeam;
+	private void Start() { this.SetupStructure(0, 0, 0, 0, 1000, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, this.inspectorTeam); }
+
+	private void Update() {
+		base.Update();
+		// TODO: Make enemy team win if this is dead.
+	}
+}
