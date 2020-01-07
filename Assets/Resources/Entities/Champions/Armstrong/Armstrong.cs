676338427a157bb54a15eb516c069cfb9d4e425b
T3rrabyte <tjmartin2003@gmail.com>
2020-01-07 12:50:41 -0600

    Armstrong start.

---
+++ Assets/Resources/Entities/Champions/Armstrong/Armstrong.cs
@@ -0,0 +1,21 @@
+﻿public class Armstrong : Champion {
+	/*
+	private System.Collections.Generic.Dictionary<Champion, int> _marks;
+	private void Start() {
+		this._marks = new System.Collections.Generic.Dictionary<Champion, int>();
+		foreach (Champion champion in MatchManager.Instance.champions) {
+			// Add
+		}
+
+		this.SetupChampion(80, 4, 0, 0, 800, 100, 4, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.9, 0.05, 35, 0, 0, 0, 0, 0, 0, 0, 30, 0, "Captain Armstrong", UnityEngine.Resources.Load<UnityEngine.Sprite>("Entities/Champions/Armstrong/Icon"),
+						   new Ability(0, this, "Focus Fire", "Armstrong applies marks to targets that he basic attacks. When an enemy has 3 marks, they take additional damage from Captain Armstrong's attacks.", 0, UnityEngine.Resources.Load<UnityEngine.Sprite>("Entities/Champions/Amstrong/PassiveIcon"), () => {
+																																																																													  
+																																																																												  }), 
+						   new Ability(),
+						   new Ability(),
+						   new Ability(),
+						   new Ability()
+						  );
+	}
+	*/
+}
--- Assets/Resources/Entities/Structures/Tower/Tower.cs
+++ Assets/Resources/Entities/Structures/Tower/Tower.cs
@@ -5,7 +5,7 @@
 	public Inhibitor protectorInhibitorMiddle;
 	public Inhibitor protectorInhibitorBottom;
 
-	private void Start() { this.SetupStructure(100, 0, 0, 0, 5000, 0, 0, 0, 0, 0, 0, 0, 100, 0, 100, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 30, 0, this.inspectorTeam); }
+	private void Start() { this.SetupStructure(300, 0, 0, 0, 10000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 30, 0, this.inspectorTeam); }
 
 	private new void Update() {
 		base.Update();
@@ -13,8 +13,12 @@
 			this.health = this.vitality.CurrentValue;
 			this.gameObject.SetActive(true);
 		}
+
 		if (!this.basicAttackTarget) { this.BasicAttackCommand(this.ClosestEntityInRange(true, false, false, false, false, true, this.range.CurrentValue)); } // Non-champion, non-structure enemies in range.
+
 		if (!this.basicAttackTarget) { this.BasicAttackCommand(this.ClosestEntityInRange(true, false, false, true, false, true, this.range.CurrentValue)); } // Non-structure enemies in range.
-																																							 // TODO: Target enemy champion if they attack an ally champion and the current target isn't a champion.
+
+		// TODO: Target enemy champion if they attack an ally champion and the current target isn't a champion.
+		// TODO: Leveling up.
 	}
 }
