6f2efdbf91bd3083c19947c64f5e1e6cdd8f37cd
T3rrabyte <tjmartin2003@gmail.com>
2020-01-15 13:50:51 -0600

    Status effects.

--- Assets/Scripts/StatusEffect.cs
+++ Assets/Scripts/StatusEffect.cs
@@ -1,22 +1,32 @@
 ﻿public class StatusEffect {
-	public enum StatusEffectType { slow, stun, immobilize, silence }
+	public enum StatusEffectType { immobilize, silence }
 
 	public StatusEffectType type;
 	private System.DateTime startTime;
-	private double severity;
-	private double duration;
+	public double severity;
+	private double originalSeverity;
+	private System.TimeSpan duration;
+	private bool decaying;
+	private bool reverseDecaying;
 	private Entity target;
 
-	public StatusEffect(StatusEffectType type, double severity, double duration, Entity target) {
+	public StatusEffect(StatusEffectType type, double severity, double duration, bool decaying, bool reverseDecaying, Entity target) {
 		this.type = type;
 		this.startTime = System.DateTime.Now;
 		this.severity = severity;
-		this.duration = duration;
+		this.originalSeverity = severity;
+		this.duration = System.TimeSpan.FromSeconds(duration);
 		this.target = target;
+		this.decaying = decaying;
+		this.reverseDecaying = reverseDecaying;
 		target.currentStatusEffects.Add(this);
 	}
 
 	private void Update() {
-		if (System.TimeSpan.FromSeconds(this.duration) < System.DateTime.Now - this.startTime) { this.target.currentStatusEffects.Remove(this); }
+		System.TimeSpan timePassed = System.DateTime.Now - this.startTime;
+		System.TimeSpan remainingTime = this.duration - timePassed;
+		if (remainingTime < System.TimeSpan.Zero) { this.target.currentStatusEffects.Remove(this); }
+
+		if (this.decaying) { this.severity = this.originalSeverity - ((timePassed.TotalSeconds / this.duration.TotalSeconds) * this.originalSeverity); } else if (this.reverseDecaying) { this.severity = (timePassed.TotalSeconds / this.duration.TotalSeconds) * this.originalSeverity; }
 	}
 }
